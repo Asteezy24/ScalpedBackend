b0608d655038a8696480cb41b0a12ea86e40a01c
Asteezy24 <alxtv1026@gmail.com>
2020-12-16 18:53:42 -0500

    removing log from push notifications

--- helpers/notify.js
+++ helpers/notify.js
@@ -33,8 +33,8 @@ function createNote () {
 
 function sendPushNotification (note, token) {
   apnProvider.send(note, token).then((response) => {
-    console.log(response.sent)
-    console.log(response.failed)
+    //console.log(response.sent)
+    //console.log(response.failed)
     // response.sent: Array of device tokens to which the notification was sent succesfully
     // response.failed: Array of objects containing the device token (`device`) and either an `error`, or a `status` and `response` from the API
   })
